8fd6fdda37fdfa9dd9b1560bc184dc2287f4b37d
nick-funk <code@nickfunk.com>
2025-02-25 00:43:10 -0700

    compute nodes, neighbours for grid

--- lib/new/grid.spec.ts
+++ lib/new/grid.spec.ts
@@ -17,7 +17,7 @@ it("grid can draw out to console", () => {
   );
 });
 
-it("grid width and height calculated correctly", () => {
+it("grid width and height are calculated correctly", () => {
   const traversal = [
     [0, 0, 0, 1, 0],
     [0, 1, 0, 1, 0],
@@ -28,4 +28,47 @@ it("grid width and height calculated correctly", () => {
 
   expect(grid.width).toEqual(5);
   expect(grid.height).toEqual(3);
+});
+
+it("grid nodes are calculated correctly", () => {
+  const traversal = [
+    [0, 1, 0],
+    [0, 0, 0],
+    [1, 0, 0]
+  ];
+
+  const grid = new Grid(traversal);
+
+  const centreNode = grid.getNode(1, 1);
+  expect(centreNode).toBeDefined();
+  expect(centreNode?.topLeft).toEqual(true);
+  expect(centreNode?.top).toEqual(false);
+  expect(centreNode?.topRight).toEqual(true);
+  expect(centreNode?.right).toEqual(true);
+  expect(centreNode?.bottomRight).toEqual(true);
+  expect(centreNode?.bottom).toEqual(true);
+  expect(centreNode?.bottomLeft).toEqual(false);
+  expect(centreNode?.left).toEqual(true);
+
+  const topLeftNode = grid.getNode(0, 0);
+  expect(topLeftNode).toBeDefined();
+  expect(topLeftNode?.topLeft).toEqual(false);
+  expect(topLeftNode?.top).toEqual(false);
+  expect(topLeftNode?.topRight).toEqual(false);
+  expect(topLeftNode?.right).toEqual(false);
+  expect(topLeftNode?.bottomRight).toEqual(true);
+  expect(topLeftNode?.bottom).toEqual(true);
+  expect(topLeftNode?.bottomLeft).toEqual(false);
+  expect(topLeftNode?.left).toEqual(false);
+
+  const topRightNode = grid.getNode(2, 0);
+  expect(topRightNode).toBeDefined();
+  expect(topRightNode?.topLeft).toEqual(false);
+  expect(topRightNode?.top).toEqual(false);
+  expect(topRightNode?.topRight).toEqual(false);
+  expect(topRightNode?.right).toEqual(false);
+  expect(topRightNode?.bottomRight).toEqual(false);
+  expect(topRightNode?.bottom).toEqual(true);
+  expect(topRightNode?.bottomLeft).toEqual(true);
+  expect(topRightNode?.left).toEqual(false);
 });
--- lib/new/grid.ts
+++ lib/new/grid.ts
@@ -13,19 +13,169 @@ export class GridPos {
   }
 }
 
+interface NodeProps {
+  x: number;
+  y: number;
+  topLeft: boolean;
+  top: boolean;
+  topRight: boolean;
+  right: boolean;
+  bottomRight: boolean;
+  bottom: boolean;
+  bottomLeft: boolean;
+  left: boolean;
+}
+
+export class Node {
+  public readonly x: number;
+  public readonly y: number;
+
+  public topLeft: boolean;
+  public top: boolean;
+  public topRight: boolean;
+  public right: boolean;
+  public bottomRight: boolean;
+  public bottom: boolean;
+  public bottomLeft: boolean;
+  public left: boolean;
+
+  constructor({
+    x,
+    y,
+    topLeft,
+    top,
+    topRight,
+    right,
+    bottomRight,
+    bottom,
+    bottomLeft,
+    left,
+  }: NodeProps) {
+    this.x = x;
+    this.y = y;
+
+    this.topLeft = topLeft;
+    this.top = top;
+    this.topRight = topRight;
+    this.right = right;
+    this.bottomRight = bottomRight;
+    this.bottom = bottom;
+    this.bottomLeft = bottomLeft;
+    this.left = left;
+  }
+}
+
 export class Grid {
-  public traversal: number[][];
   public readonly width: number;
   public readonly height: number;
 
+  public rawTraversal: number[][];
+  public grid: Node[][];
+
   constructor(traversal: number[][]) {
-    const { traversal: clone, width, height }= this.cloneTraversal(traversal);
-    this.traversal = clone;
+    const {
+      traversal: rawTraversal,
+      width,
+      height,
+    } = this.cloneTraversal(traversal);
+    this.rawTraversal = rawTraversal;
     this.width = width;
     this.height = height;
+
+    this.grid = this.computeGrid(this.width, this.height);
   }
 
-  private cloneTraversal(traversal: number[][]) {
+  public computeGrid(width: number, height: number) {
+    const grid: Node[][] = [];
+
+    for (let y = 0; y < height; y++) {
+      const row: Node[] = [];
+
+      for (let x = 0; x < width; x++) {
+        const topLeft = this.getValueFromTraversal(
+          x - 1,
+          y - 1,
+          this.rawTraversal,
+          width,
+          height
+        );
+        const top = this.getValueFromTraversal(x, y - 1, this.rawTraversal, width, height);
+        const topRight = this.getValueFromTraversal(
+          x + 1,
+          y - 1,
+          this.rawTraversal,
+          width,
+          height
+        );
+        const right = this.getValueFromTraversal(x + 1, y, this.rawTraversal, width, height);
+        const bottomRight = this.getValueFromTraversal(
+          x + 1,
+          y + 1,
+          this.rawTraversal,
+          width,
+          height
+        );
+        const bottom = this.getValueFromTraversal(
+          x,
+          y + 1,
+          this.rawTraversal,
+          width,
+          height
+        );
+        const bottomLeft = this.getValueFromTraversal(
+          x - 1,
+          y + 1,
+          this.rawTraversal,
+          width,
+          height
+        );
+        const left = this.getValueFromTraversal(x - 1, y, this.rawTraversal, width, height);
+
+        const node = new Node({
+          x,
+          y,
+          topLeft: topLeft === Traversal.GROUND,
+          top: top === Traversal.GROUND,
+          topRight: topRight === Traversal.GROUND,
+          right: right === Traversal.GROUND,
+          bottomRight: bottomRight === Traversal.GROUND,
+          bottom: bottom === Traversal.GROUND,
+          bottomLeft: bottomLeft === Traversal.GROUND,
+          left: left === Traversal.GROUND,
+        });
+
+        row.push(node);
+      }
+
+      grid.push(row);
+    }
+
+    return grid;
+  }
+
+  public getNode(x: number, y: number): Node | null {
+    if (x < 0 || x >= this.width || y < 0 || y >= this.height) {
+      return null;
+    }
+
+    return this.grid[y][x];
+  }
+
+  public getValueFromTraversal(
+    x: number,
+    y: number,
+    traversal: number[][],
+    width: number,
+    height: number
+  ): number | null {
+    if (x < 0 || x >= width || y < 0 || y >= height) {
+      return null;
+    }
+
+    return traversal[y][x];
+  }
+
+  public cloneTraversal(traversal: number[][]) {
     let copy: number[][] = [];
     let width: number = 0;
 
@@ -42,13 +192,13 @@ export class Grid {
       copy.push(newRow);
     }
 
-    return { traversal: copy, width, height: traversal.length }
+    return { traversal: copy, width, height: traversal.length };
   }
 
   public toString(): string {
     const lines: string[] = [];
 
-    for (const row of this.traversal) {
+    for (const row of this.rawTraversal) {
       lines.push(row.join(", "));
     }
 
@@ -56,15 +206,17 @@ export class Grid {
   }
 
   private positionIsValid(pos: GridPos) {
-    return pos.x >= 0 && pos.x < this.width && pos.y >= 0 && pos.y < this.height;
+    return (
+      pos.x >= 0 && pos.x < this.width && pos.y >= 0 && pos.y < this.height
+    );
   }
 
   public traverse(start: GridPos, end: GridPos) {
     if (!this.positionIsValid(start)) {
-      throw new Error(`(${start.x}, ${start.y}) is out of bounds of grid (w: ${this.width}, h: ${this.height})`);
+      throw new Error(`(${start.x}, ${start.y}) is out of bounds of grid`);
     }
     if (!this.positionIsValid(end)) {
-      throw new Error(`(${end.x}, ${end.y}) is out of bounds of grid (w: ${this.width}, h: ${this.height})`);
+      throw new Error(`(${end.x}, ${end.y}) is out of bounds of grid`);
     }
   }
 }
